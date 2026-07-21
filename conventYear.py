9cfb7ae66540d0a2c3853b06c27aa4f751544d6e
410-29-phunisra <54645@yupparaj.ac.th>
2026-07-21 13:34:39 +0700

    Update conventYear.py

--- conventYear.py
+++ conventYear.py
@@ -2,5 +2,5 @@ import streamlit as gp
 gp.title("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ.")
 
 tn_year=gp.number_input("กรอกปี พ.ศ. ที่ต้องการแปลง",value=2569)
-ab_year=tn_yaer-543
+ab_year=tn_year-543
 gp.header(f"ปี ค.ศ. คือ : {ab_yaer}")
